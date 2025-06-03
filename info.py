0812dfb213fe5911b36abd9b5e75240af2c392c8
Farook13 <umarulfarook787@gmail.com>
2025-06-03 16:33:52 +0530

    Update info.py

--- info.py
+++ info.py
@@ -18,7 +18,7 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'TheBlackBot')
 API_ID = int(environ.get('API_ID', '29450452'))
 API_HASH = environ.get('API_HASH', '54759945ff88b52777eec9a455944d31')
-BOT_TOKEN = environ.get('BOT_TOKEN', "7693803634:AAFIWfW8gfzMYv-G5-I9hAnge1mYFVkspio")
+BOT_TOKEN = environ.get('BOT_TOKEN', "7857321740:AAHSUfjwO3w6Uffmxm9vCUMl36FtXl5-r6w")
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002332361885'))
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5032034594').split()]
 
